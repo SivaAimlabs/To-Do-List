cbd24fc5159c49f01f64eafdd9cd42437885caf5
SivaAimlabs <hemanth.yerragondu@aimlabs.in>
2022-06-22 11:49:28 +0530

    Update script.js
    Completed updateli Function

--- script.js
+++ script.js
@@ -10,8 +10,7 @@ function deleteli(TaskNumber) {
 function showEditOptions(TaskNumber) {
     console.log("show edit options")
     var TaskName = $("#" + TaskNumber + " .task").html()
-    $("#" + TaskNumber + "").html("<input id='txtupdate_TaskNumber' value='" + TaskNumber + "'/>" +
-        "<input id='txtupdate_TaskName' value='" + TaskName + "'/>"+
+    $("#" + TaskNumber + "").html("<input id='txtupdate_TaskName' value='" + TaskName + "'/>"+
         "<button onclick = updateli('" + TaskNumber + "') > Update </button>" +
         "<button onclick = cancelupdate('" + TaskNumber + "') > Cancel </button>" +
         "<button onclick = deleteli('" + TaskNumber + "') > Delete </button>");
@@ -22,21 +21,12 @@ function updateli(TaskNumber) {
     var btneditId = "btnedit" + TaskNumber;
     var btndeleteId = "btndelete" + TaskNumber;
 
-    var CurrentRow = $("#" + TaskNumber + "");
-    var data = CurrentRow.querySelectorAll(".td-data");
+    var x = $("#txtupdate_TaskName").val()
 
-    var TaskName = data[1].querySelector("#txtupdate_TaskName").value;
-    var Status = data[2].querySelector("#txtupdate_Status").value;
-
-
-    data[0].innerHTML = TaskNumber;
-    data[1].innerHTML = TaskName;
-    data[2].innerHTML = Status;
-
-
-    var actionbtn = "<button id='" + btneditId + "' class='btn btn-info btn-xs btn-editcustomer' onclick='showeditrow(" + TaskNumber + ")'><i class='fa fa-pencil' aria-hidden='true'></i>Edit</button>" +
-        "<button id='" + btndeleteId + "' class='btn btn-danger btn-xs btn-deleteEmployee' onclick='deleteEmployeeRow(" + TaskNumber + ")'><i class='fa fa-trash' aria-hidden='true'>Delete</button>"
-    data[3].innerHTML = actionbtn;
+    $("#myTasks").html("<li id = '" + TaskNumber + "' class = 'liItem'>" +"<div class= 'task'>"+ x +"</div>"+
+    "<button id='" + btndeleteId + "' onclick =deleteli('" + TaskNumber + "')> Delete </button>" +
+    "<button id='" + btneditId + "' onclick=showEditOptions('" + TaskNumber + "')>Edit</button>" +
+    "</li>");
 }
 
 $(document).ready(function () {
